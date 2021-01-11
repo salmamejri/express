1de261385d7b8bfcdfbb0b63e6893b4e3bc05c06
mejrisalma <mejri123salma@gmail.com>
2021-01-11 14:42:12 +0100

    express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const path = require('path')
+
+
+
+app.listen(5000,(err)=>{
+    if (err){
+        throw err
+    }else{
+        console.log('server is runing?? .... oky')
+    }
+})
